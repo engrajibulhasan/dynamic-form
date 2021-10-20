ae31f4c72428d5f46481d23a4dfc5bbcbc0f7a56
engrajibulhasan <engrajibulhasan@gmail.com>
2021-10-20 23:08:33 +0600

    fontawesome icons added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fab } from "@fortawesome/free-brands-svg-icons";
+import { far } from "@fortawesome/free-regular-svg-icons";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+import Navigation from './components/Navigation';
+library.add(fab, fas, far);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navigation/>
+    </>
   );
 }
 
---
+++ src/components/Navigation.js
@@ -0,0 +1,63 @@
+import React from 'react'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import './Navigation.css';
+function Navigation() {
+    return (
+        <header className="header-area">
+            <div className="header-left">
+                 Event
+            </div>
+
+            <div className="header-middle">
+                <ul>
+                    <li>
+                        <FontAwesomeIcon icon={["fas", "plus"]} />
+                        <span>Insert</span>
+                    </li>
+
+                    <li>
+                        <FontAwesomeIcon icon={["far", "file-alt"]} />
+                        <span>New Page</span>
+                    </li>
+
+                    <li>
+                        <FontAwesomeIcon icon={["fab", "pied-piper"]} />
+                        <span>Logo</span>
+                    </li>
+
+                    <li>
+                        <FontAwesomeIcon icon={["far", "image"]} />
+                        <span>Cover</span>
+                    </li>
+
+                    <li>
+                        <FontAwesomeIcon icon={["far", "eye"]} />
+                        <span>Preview</span>
+                    </li>
+
+                    <li>
+                        <FontAwesomeIcon icon={["far", "paper-plane"]} />
+                        <span>Publish</span>
+                    </li>
+
+                    <li>
+                        <FontAwesomeIcon icon={["fas", "share-alt"]} />
+                        <span>Share</span>
+                    </li>
+
+                    <li>
+                        <FontAwesomeIcon icon={["fas", "cog"]} />
+                        <span>Settings</span>
+                    </li>
+                </ul>
+            </div>
+
+            <div className="header-right">
+                <span>Event Manage</span>
+                <FontAwesomeIcon icon={["fas", "user-circle"]} />
+            </div>
+        </header>
+    )
+}
+
+export default Navigation
