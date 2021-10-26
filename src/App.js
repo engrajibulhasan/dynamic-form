fa2ede1bf2ad206d6931f4adf275cd78c3863e00
engrajibulhasan <engrajibulhasan@gmail.com>
2021-10-26 21:43:46 +0600

    React Router Added and Started Dynamic filing

--- src/App.js
+++ src/App.js
@@ -3,13 +3,28 @@ import { fab } from "@fortawesome/free-brands-svg-icons";
 import { far } from "@fortawesome/free-regular-svg-icons";
 import { fas } from "@fortawesome/free-solid-svg-icons";
 import Body from './components/Body';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Navigation from "./components/Navigation";
+
+
 library.add(fab, fas, far);
 
 function App() {
   return (
-    <>
-      <Body/>
-    </>
+    <Router>
+      
+      <Switch>
+          <Route exact path="/" >
+            <Navigation otherPage/>
+          </Route>
+          <Route  path="/create-new-form" component={Body} />
+      </Switch>
+    </Router>
   );
 }
 
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,57 +1,95 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "./Navigation.css";
-function Navigation({ addNewPage, preview, handleUploadOpen }) {
+import { Link } from "react-router-dom";
+function Navigation({ addNewPage, preview, handleUploadOpen, otherPage }) {
+  console.log('page detail',otherPage);
   return (
     <header className="header-area container-fluid">
       <div className="header-left">Automo</div>
 
       <div className="header-middle">
         <ul>
-          <li>
-            <FontAwesomeIcon icon={["fas", "plus"]} />
-            <span>Insert</span>
-          </li>
+          {!otherPage ? (
+            <>
+              <li>
+                <FontAwesomeIcon icon={["fas", "plus"]} />
+                <span>Insert</span>
+              </li>
 
-          <li onClick={addNewPage}>
-            <FontAwesomeIcon icon={["far", "file-alt"]} />
-            <span>New Page</span>
-          </li>
+              <li onClick={addNewPage}>
+                <FontAwesomeIcon icon={["far", "file-alt"]} />
+                <span>New Page</span>
+              </li>
 
-          <li onClick={() => handleUploadOpen("logo")}>
-            <FontAwesomeIcon icon={["fab", "pied-piper"]} />
-            <span>Logo</span>
-          </li>
+              <li onClick={() => handleUploadOpen("logo")}>
+                <FontAwesomeIcon icon={["fab", "pied-piper"]} />
+                <span>Logo</span>
+              </li>
 
-          <li onClick={() => handleUploadOpen("cover photo")}>
-            <FontAwesomeIcon icon={["far", "image"]} />
-            <span>Cover</span>
-          </li>
+              <li onClick={() => handleUploadOpen("cover photo")}>
+                <FontAwesomeIcon icon={["far", "image"]} />
+                <span>Cover</span>
+              </li>
 
-          <li onClick={preview}>
-            <FontAwesomeIcon icon={["far", "eye"]} />
-            <span>Preview</span>
-          </li>
+              <li onClick={preview}>
+                <FontAwesomeIcon icon={["far", "eye"]} />
+                <span>Preview</span>
+              </li>
 
-          <li>
-            <FontAwesomeIcon icon={["far", "paper-plane"]} />
-            <span>Publish</span>
-          </li>
+              <li>
+                <FontAwesomeIcon icon={["far", "paper-plane"]} />
+                <span>Publish</span>
+              </li>
 
-          <li>
-            <FontAwesomeIcon icon={["fas", "share-alt"]} />
-            <span>Share</span>
-          </li>
+              <li>
+                <FontAwesomeIcon icon={["fas", "share-alt"]} />
+                <span>Share</span>
+              </li>
 
-          <li>
-            <FontAwesomeIcon icon={["fas", "cog"]} />
-            <span>Settings</span>
-          </li>
+              <li>
+                <FontAwesomeIcon icon={["fas", "cog"]} />
+                <span>Settings</span>
+              </li>
+            </>
+          ) : (
+            <>
+              <li>
+                <Link to="/create-new-form">
+                  <FontAwesomeIcon icon={["fas", "plus"]} />
+                  <span>Create Form</span>
+                </Link>
+              </li>
+
+              <li>
+                <Link to="/drag-and-drop">
+                  <FontAwesomeIcon icon={["fas", "grip-vertical"]} />
+                  <span>Drag and Drop</span>
+                </Link>
+              </li>
+
+              <li>
+                <Link to="/login">
+                  <FontAwesomeIcon icon={["fas", "lock"]} />
+                  <span>Login</span>
+                </Link>
+              </li>
+
+              <li>
+                <Link to="/registration">
+                  <FontAwesomeIcon icon={["far", "clipboard"]} />
+                  <span>Sign up</span>
+                </Link>
+              </li>
+            </>
+          )}
         </ul>
       </div>
 
       <div className="header-right">
-        <span>Event Manage</span>
+        <span>
+          <Link to="/"> <FontAwesomeIcon icon={["fas", "th-large"]} /> Event Manage</Link>
+        </span>
         <FontAwesomeIcon icon={["fas", "user-circle"]} />
       </div>
     </header>
